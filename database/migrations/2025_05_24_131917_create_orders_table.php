8b0c5eaec75151e1108d4e60df14657a801b17ea
chiegremoryy <chiescode@gmail.com>
2025-05-24 20:36:02 +0700

    add migration files for categories, menus, orders, and order_items tables

---
+++ database/migrations/2025_05_24_131917_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_code')->unique();
+            $table->decimal('total_amount', 10, 2);
+            $table->string('payment_method');
+            // Foreign Key ke tabel 'users' (kasir yang melayani)
+            $table->foreignId('cashier_id')->constrained('users')->onDelete('cascade');
+            $table->date('order_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2025_05_24_131930_create_order_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            // Foreign Key ke tabel 'orders'
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            // Foreign Key ke tabel 'menus'
+            $table->foreignId('menu_id')->constrained('menus')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('price_per_item', 10, 2);
+            $table->decimal('subtotal', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
