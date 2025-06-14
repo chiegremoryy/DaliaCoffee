10a6aa681d2da9cb7123be37b75ea7cb867d3ad8
BimaAfka14 <bimaafkanur@gmail.com>
2025-06-14 21:43:43 +0700

    remove stock attribute from Menu model and migration

--- database/migrations/2025_05_24_131909_create_menus_table.php
+++ database/migrations/2025_05_24_131909_create_menus_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
-            $table->integer('stock')->default(0);
             $table->string('image')->nullable();
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
