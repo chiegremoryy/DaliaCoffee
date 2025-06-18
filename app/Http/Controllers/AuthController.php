5ff939f8fd37165f2c3311e6c7a4404ba839bcbd
chiegremoryy <chiescode@gmail.com>
2025-06-18 21:51:44 +0700

    delete register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,28 +41,6 @@ class AuthController extends Controller
         ]);
     }
 
-    public function showRegisterForm()
-    {
-        return view('auth.register');
-    }
-
-    public function register(Request $request)
-    {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:6|confirmed',
-        ]);
-
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        return redirect('/login')->with('success', 'Registrasi berhasil. Silakan login.');
-    }
-
     public function logout(Request $request)
     {
         Auth::logout();
